61d7d9b17566efc1b064a19c18686acc73d149ac
Jordan Buchman <jordan.m.buchman@gmail.com>
2017-06-12 20:17:49 -0600

    Add support for animated gifs

--- index.js
+++ index.js
@@ -14,6 +14,58 @@ var fs = require('fs');
 var rimraf = require('rimraf');
 var deasync = require('deasync');
 var prompt = require('prompt');
+var gifyParse = require('gify-parse');
+
+var upload = function(){
+  var tunnel = localtunnel(5000, function(err, tunnel) {
+    if (err){
+      return;
+    }
+
+    glob(path.join("/tmp","slack_emojifier/")+"*.*", function (er, files) {
+      var emojis = []
+
+      files.sort(function(a, b) {
+        var a_match = a.match(/_(\d+)_(\d+)/);
+        var b_match = b.match(/_(\d+)_(\d+)/);
+
+        var y_sort = parseInt(a_match[2]) - parseInt(b_match[2]);
+        if (y_sort == 0){
+          return parseInt(a_match[1]) - parseInt(b_match[1]);
+        }
+        else{
+          return y_sort;
+        }
+
+      });
+      files.forEach(function(file){
+        cp.exec("convert "+ file +" -resize "+emoji_size+'x'+emoji_size+" -background 'rgba(0,0,0,0)' -gravity NorthWest -extent "+emoji_size+'x'+emoji_size+" "+file, function(err,stdout,stderr){
+          cp.execSync("convert "+ file +" -resize "+128+'x'+128+" "+file);
+        });
+
+        var name = path.basename(file).split('.')[0]
+
+        emojis.push({
+          'src': tunnel.url+"/"+path.basename(file),
+          'name': name
+          })
+      });
+      var line = "";
+      files.forEach(function(file){
+        var emoji_name = path.basename(file).split('.')[0];
+        if (/_1_\d+$/.test(emoji_name)){
+          process.stdout.write(os.EOL)
+        }
+        process.stdout.write(":"+emoji_name+":");
+      });
+      process.stdout.write(os.EOL)
+      emojipacks.upload(argv.s, argv.e, argv.p, emojis)
+      return;
+    });
+  })
+}
+
+
 
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
@@ -82,6 +134,8 @@ prompt.start();
 promptGet = deasync(prompt.get);
 argv = promptGet(schema);
 
+glob_sync = deasync(glob);
+
 
 if(fs.existsSync(path.join('/tmp','slack_emojifier'))){
   rimraf.sync(path.join('/tmp','slack_emojifier'));
@@ -106,50 +160,37 @@ var dimensions = sizeOf(argv.i);
 
 var emoji_size = parseInt(dimensions.width/num_cols);
 
-convert = cp.execSync('convert '+ argv.i +' -crop '+emoji_size+'x'+emoji_size+' -set filename:tile "%[fx:ceil(page.x/'+emoji_size+'+1)]_%[fx:ceil(page.y/'+emoji_size+'+1)]" +repage +adjoin "'+path.join('/tmp','slack_emojifier',name)+'_%[filename:tile].png"')
-var tunnel = localtunnel(5000, function(err, tunnel) {
-    if (err){
-      return;
-    }
 
-  glob(path.join("/tmp","slack_emojifier/")+"*.*", function (er, files) {
-    var emojis = []
+if (argv.i.endsWith('.gif')){
+  var gifInfo = gifyParse.getInfo(fs.readFileSync(argv.i));
+  var delay = parseInt(gifInfo.duration / gifInfo.images.length / 10);
+  var num_frames = gifInfo.images.length;
+  console.log(delay);
+  console.log(gifInfo.images[0].delay)
+  convert = cp.execSync('convert '+ argv.i +' +repage +adjoin '+path.join('/tmp','slack_emojifier',name)+'_%d.gif');
 
-    files.sort(function(a, b) {
-      var a_match = a.match(/_(\d+)_(\d+)/);
-      var b_match = b.match(/_(\d+)_(\d+)/);
-
-      var y_sort = parseInt(a_match[2]) - parseInt(b_match[2]);
-      if (y_sort == 0){
-        return parseInt(a_match[1]) - parseInt(b_match[1]);
-      }
-      else{
-        return y_sort;
-      }
-
-    });
+  glob_sync(path.join("/tmp","slack_emojifier/")+"*.*", function (er, files) {
     files.forEach(function(file){
-      cp.exec("convert "+ file +" -resize "+emoji_size+'x'+emoji_size+" -background 'rgba(0,0,0,0)' -gravity NorthWest -extent "+emoji_size+'x'+emoji_size+" "+file, function(err,stdout,stderr){
-        cp.execSync("convert "+ file +" -resize "+128+'x'+128+" "+file);
-      });
-
       var name = path.basename(file).split('.')[0]
-
-      emojis.push({
-        'src': tunnel.url+"/"+path.basename(file),
-        'name': name
-        })
+      var convert = cp.execSync('convert '+ file +' -coalesce -crop '+emoji_size+'x'+emoji_size+' -set filename:tile "%[fx:ceil(page.x/'+emoji_size+'+1)]_%[fx:ceil(page.y/'+emoji_size+'+1)]" +repage +adjoin "'+path.join('/tmp','slack_emojifier',name)+'_%[filename:tile].gif"');
+      var remove = cp.execSync('rm '+file);
+      console.log(file)
     });
-    var line = "";
-    files.forEach(function(file){
-      var emoji_name = path.basename(file).split('.')[0];
-      if (/_1_\d+$/.test(emoji_name)){
-        process.stdout.write(os.EOL)
+    var num_rows = parseInt(Math.ceil(dimensions.height/emoji_size));
+    for (var col=1; col<=num_cols; col++){
+      for (var row=1; row<=num_rows; row++){
+        console.log(row,col);
+        var animate = cp.execSync('convert '+'-delay '+delay+' -dispose Background -loop 0 '+path.join("/tmp","slack_emojifier/")+"*"+col+"_"+row+".gif "+path.join("/tmp","slack_emojifier/")+name+"_"+col+"_"+row+".gif");
+        var remove = cp.execSync('rm '+path.join("/tmp","slack_emojifier/")+name+"_*_"+col+"_"+row+".gif")
       }
-      process.stdout.write(":"+emoji_name+":");
-    });
-    process.stdout.write(os.EOL)
-    emojipacks.upload(argv.s, argv.e, argv.p, emojis)
-    return;
+    }
+    upload();
+    process.exit();
   });
-})
+  console.log("done!")
+}
+
+else{
+  convert = cp.execSync('convert '+ argv.i +' -crop '+emoji_size+'x'+emoji_size+' -set filename:tile "%[fx:ceil(page.x/'+emoji_size+'+1)]_%[fx:ceil(page.y/'+emoji_size+'+1)]" +repage +adjoin "'+path.join('/tmp','slack_emojifier',name)+'_%[filename:tile].png"');
+  upload();
+}
